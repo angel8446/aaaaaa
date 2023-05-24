9b864c94d7204dbe0a27337dcc2453113c1a74eb
angel8446 <133125089+angel8446@users.noreply.github.com>
2023-05-24 08:35:01 -0500

    Update aa2.py

--- aa2.py
+++ aa2.py
@@ -1,1 +1,16 @@
+import periodictable
 
+def calcular_peso_elemento(elemento):
+    try:
+        masa_atomica = periodictable.elements.symbol(elemento).mass
+        return masa_atomica
+    except periodictable.core.periodictable.InvalidElementError:
+        return None
+
+elemento = input("Ingresa el símbolo del elemento: ")
+peso = calcular_peso_elemento(elemento)
+
+if peso is not None:
+    print("El peso del elemento {} es: {:.4f} g/mol".format(elemento, peso))
+else:
+    print("Elemento no encontrado en la tabla periódica.")
